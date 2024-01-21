4385b76c6e7059959b5cccc33647721fb6d0d9bf
fzl96 <fpsdragon333@gmail.com>
2024-01-21 17:33:59 +0700

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "@/context/auth-context.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <AuthProvider>
     <React.StrictMode>
       <App />
+      <Analytics />
     </React.StrictMode>
   </AuthProvider>
 );
